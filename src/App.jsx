e318a4d37c1bf419403d33dc045f79e441b2d513
dubsdee <84213328+dubsdee@users.noreply.github.com>
2022-06-01 18:35:22 -0400

    Added Connect to Wallet functionality in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-const App = () => {
-  return (
-    <div className="landing">
-      <h1>Welcome to My DAO</h1>
-    </div>
-  );
-};
+// Importing metamask functionality from thirdweb
+import { useAddress, useMetamask } from '@thirdweb-dev/react';
 
-export default App;
+const App = () => {
+  // use the hooks that were imported via thirdweb
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  console.log("Hi Address:", address);
+
+  // if user hasn't connected their wallet to the app,
+  // then you allow a connectWallet call
+  if (!address) {
+    return (
+      <div className = "landing">
+        <h1>Welcome to SwoleDao</h1>
+        <button onClick={connectWithMetamask} className="btn-hero">
+          connect your wallet bro
+        </button>
+      </div>
+    );
+  }
+  
+  // in the case where we have the user's address, 
+  // aka they've connected to our site
+  return (
+    <div className = "landing">
+      <h1>wallet connected, welcome back bro</h1>
+    </div>);
+}
+
+  export default App;
