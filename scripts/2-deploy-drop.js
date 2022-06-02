affb891c78a0247d260da85e161a0bef2adbebc8
dubsdee <84213328+dubsdee@users.noreply.github.com>
2022-06-01 22:22:54 -0400

    Added deploy drop.js and included 45plate jpg image in scripts/assets

---
+++ scripts/2-deploy-drop.js
@@ -0,0 +1,35 @@
+import { AddressZero } from "@ethersproject/constants";
+import sdk from "./1-initialize-sdk.js";
+import { readFileSync } from "fs";
+
+(async () => {
+    try {
+        const editionDropAddress = await sdk.deployer.deployEditionDrop({
+            // give collection a name 
+            name: "Swole DAO Gym Pass",
+            // give a description for the collection
+            description: "A DAO for gym and fitness enthusiasts.",
+            // link the image that will be held on our NFT
+            image: readFileSync("scripts/assets/45plate.jpg"),
+            // we need to pass the address of the person who is getting the proceeds 
+            // of sales of nfts in the contract.
+            // We're not charging for the mint - all funds are getting sent to the 0x0 address
+            primary_sale_recipient: AddressZero,
+        });
+
+        // this initialization returns the address of the contract
+        // we use this to initialize the contract on the thirdweb sdk
+        const editionDrop = sdk.getEditionDrop(editionDropAddress);
+
+        // with this, we can get the metadeta of our contract
+        const metadata = await editionDrop.metadata.get();
+
+        console.log(
+            "✅ Successfully deployed editionDrop contract, address:",
+            editionDropAddress,
+        );
+        console.log("✅ editionDrop metadata:", metadata);
+    } catch (error) {
+        console.log("failed to deploy editionDrop contract", error);
+    }
+})();
