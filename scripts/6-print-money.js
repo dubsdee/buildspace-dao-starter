f1cd9abb837d1c3af5fc01e51b6d41737d62d0e3
dubsdee <84213328+dubsdee@users.noreply.github.com>
2022-06-06 15:23:52 -0400

    Update 6-print-money.js
    Added token printing / minting page and parameters

---
+++ scripts/6-print-money.js
@@ -0,0 +1,20 @@
+import { Token } from "@thirdweb-dev/sdk";
+import sdk from "./1-initialize-sdk.js";
+
+// address of the ERC 20 contract printed out in 5-deploy-token
+const token = sdk.getToken("0xc43cf0577E53Ba0b1854bD0196388B1282fde02b");
+
+(async () => {
+    try {
+        // what is the max token supply?
+        const amount = 1000000;
+        // interact to mint tokens
+        await Token.mintToSelf(amount);
+        const totalSupply = await Token.totalSuppy();
+
+        // print out how many of the tokens are issued
+        console.log("There are now ", totalSupply.displayValue, "$CURLS in circulation.");
+    }   catch (error) {
+        console.error("Failed to print money", error);
+    }
+})();
