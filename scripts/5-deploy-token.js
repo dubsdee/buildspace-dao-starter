9dd0f6a24f942807a17505846a7ea66457d0e18c
dubsdee <84213328+dubsdee@users.noreply.github.com>
2022-06-06 14:03:49 -0400

    Update 5-deploy-token.js
    Updated token deployment contract

---
+++ scripts/5-deploy-token.js
@@ -0,0 +1,19 @@
+import { AddressZero } from "@ethersproject/constants";
+import sdk from "./1-initialize-sdk.js";
+
+(async () => {
+    try {
+        // deploy a standard erc-20 contract
+        const tokenAddress = await sdk.deployer.deployToken({
+            // what is the token name
+            name: "SwoleDAO Governance Token",
+            // token symbol
+            symbol: "CURLS",
+            // this will be in case we want to sell the token - we don'e so send to zero address
+            primary_sale_recipient: AddressZero,
+        });
+        console.log("Successfully deployed token module, address: ", tokenAddress,);
+    }   catch (error) {
+        console.error("failed to deploy token module", error);
+    }
+})();
