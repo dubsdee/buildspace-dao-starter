3caef433c1ad85b20f6da1679a1be2247292d6ce
dubsdee <84213328+dubsdee@users.noreply.github.com>
2022-06-06 12:57:11 -0400

    Update App.jsx
    Updated front end to include minting button, and landing page options for mint status / connection status

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,13 @@ const App = () => {
   const address = useAddress();
   const connectWithMetamask = useMetamask();
   console.log("Hi Address:", address);
+  
   // initialize the editionDrop contract
   const editionDrop = useEditionDrop("0xA3BBB310D49b1B25B92D2eBD8d06D171A2B04296");
   // state variable for us to know if user has the NFT
   const [hasClaimedNFT, setHasClaimedNFT ] = useState(false);
+  // is claiming lets us keep a loading state while NFT is minting
+  const [isClaiming, setIsClaiming] = useState(false);
 
   useEffect(() => {
     // if they don't have a connected wallet, exit
@@ -37,6 +40,26 @@ const App = () => {
     checkBalance();
   }, [address, editionDrop]);
 
+  // 
+  const mintNft = async () => {
+    try {
+      // initializes the loading state to set up signer
+      setIsClaiming(true);
+      // mint the NFT to users wallet when they click the onclick button
+      // passing 0 for the token id and 1 for the mint number
+      await editionDrop.claim("0", 1);
+      console.log('Successfully minted! Check it out on OpenSea: https://testnest.opensea.io/assets/${editionDrop.getAddress()}/0');
+      // stops the loading state and sets to true for successful mint
+      setHasClaimedNFT(true);
+    } catch(error) {
+      setHasClaimedNFT(false);
+      console.error("failed to mint NFT", error);
+    } finally {
+      setIsClaiming(false);
+    }
+  };
+
+
   // if user hasn't connected their wallet to the app,
   // then you allow a connectWallet call
   if (!address) {
@@ -50,12 +73,27 @@ const App = () => {
     );
   }
   
-  // in the case where we have the user's address, 
-  // aka they've connected to our site
-  return (
-    <div className = "landing">
-      <h1>wallet connected, welcome back bro</h1>
-    </div>);
-}
+  if (hasClaimedNFT) {
+    return (
+      <div className='member-page'>
+        <h1>SwoleDAO Member Page</h1>
+        <p>Congrats on being a member bro</p>
+      </div>
+    );
+  };
 
+  // render mint nft screen
+  return (
+    <div className="mint-nft">
+      <h1>Mint your free SwoleDAO Membership NFT!</h1>
+      <button
+      disabled={isClaiming}
+      onClick={mintNft}
+      >
+        {isClaiming ? "Minting..." : "Mint your free NFT!"}
+      </button>
+    </div>
+  );
+}
+  
   export default App;
